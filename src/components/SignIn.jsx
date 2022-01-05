5eee6973700112d4b4478116d48427e8d39666c4
fedosieievaa <nastyabarabakh@gmail.com>
2022-01-05 21:23:02 +0200

    "localStorage" and "Remember me" added

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,22 +1,52 @@
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../assets/img/padlock.png";
 
 export const SignIn = () => {
-    
+    const [userEmail, setUserEmail] = useState("");
+    const [userPassword, setUserPassword] = useState("");
+    const [remember, setRemember] = useState("off");
+
+    const email = localStorage.getItem("email");
+    const password = localStorage.getItem("password");
+
+    const getValid = (e) => {
+        e.preventDefault();
+        if(userEmail === email && userPassword === password){
+            alert("SUCCESS: account login completed 🙂");
+            setUserEmail("");
+            setUserPassword("");
+        }else{
+            alert("FAILURE: wrong email or password 🤥");
+        }
+
+        if(remember === "on") {
+            localStorage.setItem("userEmail", userEmail);
+            localStorage.setItem("userPassword", userPassword);
+        }
+
+    }
+    const func =() => {
+        setUserEmail(localStorage.getItem("userEmail"));
+        setUserPassword(localStorage.getItem("userPassword"));
+    }
+
+    useEffect(()=>func(),[])
+
     return(
         <div className="container">
             <div className="logo">
                 <img src={Logo} alt="Logo" />
             </div>
             <h2 className="title">Sign in</h2>
-            <form  className="form" action="#">
-                <input className="form_input" type="email" placeholder="Email Address *"/>
-                <input className="form_input" type="password" placeholder="Password *"/>
+            <form  className="form" action="#" onSubmit={getValid}>
+                <input className="form_input" type="email" placeholder="Email Address *" value={userEmail} onChange={(e)=>setUserEmail(e.target.value)} required/>
+                <input className="form_input" type="password" placeholder="Password *" value={userPassword} onChange={(e)=>setUserPassword(e.target.value)} required/>
                 <div className="checkbox_container">
-                    <input className="form_check" type="checkbox"  id="remember"/>
+                    <input className="form_check" type="checkbox" id="remember" onChange={(e)=>setRemember(e.target.value)}/>
                     <label htmlFor="remember">Remember me</label>
                 </div>
-                <button className="btn__sign-in-up">SIGN UP</button>
+                <button className="btn__sign-in-up">SIGN IN</button>
             </form>
             <div className="container_link-signup">
                 <a className="container_link" href="/">Forgot password?</a>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,22 +1,39 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../assets/img/padlock.png";
 
 
 export const SignUp = () => {
-    
+    const [name, setName] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const getUser = (e) => {
+        e.preventDefault();
+        localStorage.setItem("name", name);
+        localStorage.setItem("lastname", lastname);
+        localStorage.setItem("email", email);
+        localStorage.setItem("password", password);
+        setName("");
+        setLastname("");
+        setEmail("");
+        setPassword("");
+    }
+
     return(
         <div className="container">
             <div className="logo">
                 <img src={Logo} alt="Logo" />
             </div>
             <h2 className="title">Sign up</h2>
-            <form className="form" action="#">
+            <form className="form" action="#" onSubmit={getUser}>
                 <div className="form_name">
-                    <input className="form_input_sm" type="text" placeholder="First Name *"/>
-                    <input className="form_input_sm" type="text" placeholder="Last Name *"/>
+                    <input className="form_input_sm" type="text" placeholder="First Name *" value={name} onChange={(e)=>setName(e.target.value)} required/>
+                    <input className="form_input_sm" type="text" placeholder="Last Name *" value={lastname} onChange={(e)=>setLastname(e.target.value)} required/>
                 </div>
-                <input className="form_input" type="email" placeholder="Email Address *"/>
-                <input className="form_input" type="password" placeholder="Password *"/>
+                <input className="form_input" type="email" placeholder="Email Address *" value={email} onChange={(e)=>setEmail(e.target.value)} required/>
+                <input className="form_input" type="password" placeholder="Password *" value={password} onChange={(e)=>setPassword(e.target.value)} required/>
                 <div className="checkbox_container">
                     <input className="form_check" type="checkbox"  id="remember"/>
                     <label htmlFor="remember">I want to receive inspiration, marketing promotions and updates via email.</label>
